ac686391ab106c610e2ce6a60c7ee22d15b9b163
k3ppl3r <bnknqh@umsystem.edu>
2025-10-24 20:32:00 -0500

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -7,6 +7,7 @@ API_KEY = "6S6GQ8K8UNOSFDJS"
 BASE_URL = "https://www.alphavantage.co/query"
 
 while True:
+    #user inputs
     stock_symbol = input("Enter stock symbol (e.g., AAPL, MSFT): ").upper()
 
     print("\nEnter chart type \n1.Line\n2.Bar\n")
@@ -18,21 +19,44 @@ while True:
     start_date = input("\nEnter start date (YYYY-MM-DD): ")
     end_date = input("Enter end date (YYYY-MM-DD): ")
 
-    continue_prompt = input("\nDo you want to continue? (yes/no): \n").lower()
-    if continue_prompt != 'yes':
-        print("Exiting the program.")
-        break
-
-# Determine function choice to API parameters
+#determine function choice to API parameters
     if function_choice == '1':
         function = "TIME_SERIES_INTRADAY"
         interval = "60min"
     elif function_choice == '2':
         function = "TIME_SERIES_DAILY"
+        interval = None
     elif function_choice == '3':
         function = "TIME_SERIES_WEEKLY"
+        interval = None
     elif function_choice == '4':
         function = "TIME_SERIES_MONTHLY"
+        interval = None
     else:
-        print("Invalid choice. Exiting.")
-        exit()
+        print("Invalid choice. Please try again.")
+        continue
+
+    #api parameters
+    params = {
+        "function": function,
+        "symbol": stock_symbol,
+        "apikey": API_KEY
+    }
+    if interval:
+        params["interval"] = interval
+
+    print("\nFetching data from Alpha Vantage...")
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+#check if data is valid
+
+#filter data
+
+#create chart
+
+#continue or exit
+    continue_prompt = input("\nDo you want to continue? (yes/no): \n").lower()
+    if continue_prompt != 'yes':
+        print("Exiting the program.")
+        break
